9547059c9e845f6b59f562f7ff6b142355aabb3c
Massimiliano Asti <massimiliano.asti@dgsspa.com>
2023-01-09 16:25:13 +0100

    Update config

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets/"),
+      // components
+      "@comp-app": path.resolve(__dirname, "src/components/app/"),
+      "@comp-game": path.resolve(__dirname, "src/components/game/"),
+      "@comp-main": path.resolve(__dirname, "src/components/main/"),
+      // common
+      "@comm-enums": path.resolve(__dirname, "src/common/enums/")
+    },
+  }
+};
